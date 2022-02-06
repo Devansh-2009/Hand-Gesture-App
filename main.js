67c94dd7877d213738082e6a25b873d3f0510561
Devansh-2009 <87084391+Devansh-2009@users.noreply.github.com>
2022-02-06 10:44:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-https://teachablemachine.withgoogle.com/models/NPQR0AmbE/
 prediction_1=""
 prediction_2=""
 
@@ -17,7 +16,7 @@ function take_snapshot(){
     });
 }
 console.log('ml5 version:', ml5.version);
-classifier=ml5.imageClassifier('',modelLoaded);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/NPQR0AmbE/',modelLoaded);
 function modelLoaded(){
     console.log('Model Loaded');
 } 
@@ -28,3 +27,48 @@ function speak(){
     var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
     synth.speak(utterThis);
 }
+function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById("result_emotion_name1").innerHTML = results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+    prediction_1 = results[0].label;
+    prediction_2 = results[1].label;
+    speak();
+    if(results[0].label == "thumbs up")
+    {
+      document.getElementById("update_emoji1").innerHTML = "&#128079;";
+    }
+    if(results[0].label == "thumbs down")
+    {
+      document.getElementById("update_emoji1").innerHTML = "&#128078;";
+    }
+    if(results[0].label == "clapping")
+    {
+      document.getElementById("update_emoji1").innerHTML = "&#128077;";
+    }
+
+    if(results[1].label == "thumbs up")
+    {
+      document.getElementById("update_emoji2").innerHTML = "&#128079;";
+    }
+    if(results[1].label == "thumbs down")
+    {
+      document.getElementById("update_emoji2").innerHTML = "&#128078;";
+    }
+    if(results[1].label == "clapping")
+    {
+      document.getElementById("update_emoji2").innerHTML = "&#128077;";
+    }
+  }
+}
+
